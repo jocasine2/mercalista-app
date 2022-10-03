c54fd876339eb6df65bf1da47cc8352923af7383
joaquim.neto <joaquim.neto@ati.to.gov.br>
2022-10-03 11:29:57 -0300

    🚑 Corrige erro no componenete da lista quando a palavra buscada é vazia

--- myapp/src/app/shared/components/data-table/data-table.component.ts
+++ myapp/src/app/shared/components/data-table/data-table.component.ts
@@ -22,6 +22,7 @@ export class DataTableComponent implements OnInit {
 
   @ViewChild('search', { static: false }) search: any;
 
+
   constructor(private http: HttpClient) {
     this.columns = [
       { name: 'Name' },
@@ -29,40 +30,52 @@ export class DataTableComponent implements OnInit {
       { name: 'Genre' }
     ];
 
+    this.getList()
+
+  }
+
+  getList() {
     this.http.get<Data>('../../../assets/movies.json')
       .subscribe((res) => {
         this.rows = res.movies;
       });
-
   }
 
   ngOnInit() {
-    // populate datatable rows
-    this.data = this.rows;
-    // copy over dataset to empty object
-    this.filteredData = this.rows;
+ // populate datatable rows
+ this.data = this.rows;
+ // copy over dataset to empty object
+ this.filteredData = this.rows;
   }
 
   filterDatatable(event){
-     // get the value of the key pressed and make it lowercase
-    let val = event.target.value.toLowerCase();
-    // get the amount of columns in the table
-    let colsAmt = this.columns.length;
-    // get the key names of each column in the dataset
-    let keys = Object.keys(this.rows[0]);
-    // assign filtered matches to the active datatable
 
-    this.data = this.rows.filter(function(item){
-      // iterate through each row's column data
-      for (let i=0; i<colsAmt; i++){
-        // check for a match
-        if (item[keys[i]].toString().toLowerCase().indexOf(val) !== -1 || !val){
-          // found match, return true to add to result set
-          return true;
+    // get the value of the key pressed and make it lowercase
+    let val = event.target.value.toLowerCase();
+
+    if(!val){
+      this.getList()
+    }else if(this.rows.length>0){
+      // get the amount of columns in the table
+      let colsAmt = this.columns.length;
+      // get the key names of each column in the dataset
+      console.log(val);
+      let keys = Object.keys(this.rows[0]);
+      // assign filtered matches to the active datatable
+
+      this.data = this.rows.filter(function(item){
+        // iterate through each row's column data
+        for (let i=0; i<colsAmt; i++){
+          // check for a match
+          if (item[keys[i]].toString().toLowerCase().indexOf(val) !== -1 || !val){
+            // found match, return true to add to result set
+            return true;
+          }
         }
-      }
-    });
-    // whenever the filter changes, always go back to the first page
-    this.rows = this.data;
+      });
+      // whenever the filter changes, always go back to the first page
+      this.rows = this.data;
+    }
+
   }
 }
